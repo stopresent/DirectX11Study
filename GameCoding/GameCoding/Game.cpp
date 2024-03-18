756d36c4b3145feba38a086a545802d942e5a5c8
stopresent <86364202+stopresent@users.noreply.github.com>
2024-03-19 00:29:46 +0900

    [ADD] 기본 프레임워크

---
+++ GameCoding/GameCoding/Game.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include "Game.h"
+
+Game::Game()
+{
+}
+
+Game::~Game()
+{
+}
+
+void Game::Init(HWND hwnd)
+{
+	_hwnd = hwnd;
+	_width = GWinSizeX;
+	_height = GWinSizeY;
+
+	// TODO
+}
+
+void Game::Update()
+{
+}
+
+void Game::Render()
+{
+}
